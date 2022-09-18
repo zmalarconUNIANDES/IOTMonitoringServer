80495077d922ea7999ffb1f878a25439fb3fe0dc
Sebastián Noreña Márquez <senorena@condorlabs.io>
2022-09-18 10:38:57 -0500

    change time to ten minutes

--- control/monitor.py
+++ control/monitor.py
@@ -68,7 +68,7 @@ def analyze_data_luminosity():
     print("Calculando alerta luminosidad...")
     variable = 'luminosidad'
     data = Data.objects.filter(
-        base_time__gte=datetime.now() - timedelta(hours=1))
+        base_time__gte=datetime.now() - timedelta(minutes=10))
     aggregation = data.annotate(check_value=Avg('avg_value')) \
         .select_related('station', 'measurement') \
         .select_related('station__user', 'station__location') \
